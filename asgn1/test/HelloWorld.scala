a7f29e49489c5adc85434651a8567c9fbeeec36f
Saurav Shekhar <sauravshekhar01@gmail.com>
2015-03-12 00:46:27 +0530

    Added Parse Tree for Partial Grammer

--- asgn1/test/HelloWorld.scala
+++ asgn1/test/HelloWorld.scala
@@ -5,61 +5,60 @@ object sort
 {
 
   var (a:Int,b:Double)=(2,8,9,f(j));
-  var (a:Int,b:Double)=(2,8,9,a[j]);
-  var (a:Int,b:Double)=(2,8,9,obj.j);
-  var (a:Int,b:Double)=(2,8,9,t);
-  var (a:Int,b:Double)=(2,8,9,j);
-  var (a:Int,b:Double)=(2,8,9,j);
+  // var (a:Int,b:Double)=(2,8,9,a[j]);
+  // var (a:Int,b:Double)=(2,8,9,obj.j);
+  // var (a:Int,b:Double)=(2,8,9,t);
+  // var (a:Int,b:Double)=(2,8,9,j);
+  // var (a:Int,b:Double)=(2,8,9,j);
 
-  val (a:Int,b:Double)=(2,8,9,f(j));
-  val (a:Int,b:Double)=(2,8,9,a[j]);
-  val (a:Int,b:Double)=(2,8,9,obj.j);
-  val (a:Int,b:Double)=(2,8,9,t);
-  val (a:Int,b:Double)=(2,8,9,j);
-  val (a:Int,b:Double)=(2,8,9,j);
+  // val (a:Int,b:Double)=(2,8,9,f(j));
+  // val (a:Int,b:Double)=(2,8,9,a[j]);
+  // val (a:Int,b:Double)=(2,8,9,obj.j);
+  // val (a:Int,b:Double)=(2,8,9,t);
+  // val (a:Int,b:Double)=(2,8,9,j);
+  // val (a:Int,b:Double)=(2,8,9,j);
 
-  val a:String=4;
-  var we:Boolean=4;
+  //val a:String=42;
+  // var we:Boolean=4;
 
+  //a = 23;
+  //a = 6 + 5;
 
+  //a= (5 + 4) * 3;;
 
+//   a=(76+90)*76 ;
+//   b=1 << (a + b);
+//   ans = msub(1 << (a + b), (minv(m.val1) * (phi[m / i] * 2 / gcd(a + b, b))) % MOD); 
+//   var (ans:Int) = (msub(1 << (a + b), (minv(m.val1) * (phi[m / i] * 2 / gcd(a + b, b))) % MOD)); 
 
-  a=5+4;
-  {
-    a=5+4;
-  }
+// if((num%2)==0) //checking whether remainder is 0 or not. 
+// {
+//   a=23;
+//   b=23;
+// }
+// else if((num%2)==0)
+// g=78;
+// else
+// {
+//   a=23;
+//   b=23;
+// }
+ // do {
+ //  a = 23;  
+ // } while(a = 23);
 
-  a=(76+90)*76 ;
-  b=1 << (a + b);
-  ans = msub(1 << (a + b), (minv(m.val1) * (phi[m / i] * 2 / gcd(a + b, b))) % MOD); 
-  var (ans:Int) = (msub(1 << (a + b), (minv(m.val1) * (phi[m / i] * 2 / gcd(a + b, b))) % MOD)); 
-
-if((num%2)==0) //checking whether remainder is 0 or not. 
-{
-  a=23;
-  b=23;
-}
-else if((num%2)==0)
-g=78;
-else
-{
-  a=23;
-  b=23;
-}
-
-
- while( a < 20 ){
-         // println( "Value of a: " + a );
-         a=a+0;
-         a = a + 1;
-      }
-
+//  while( a < 20 ){
+//          // println( "Value of a: " + a );
+//          a=a+0;
+//          a = a + 1;
+//       }
+a = 0;
 do
 {
-   println( "Value of a: " + a );
+   println( a );
    a = a + 1;
 } while( a < 20 );
 
 
-return 100 ;
+//return 100 ;
 }
